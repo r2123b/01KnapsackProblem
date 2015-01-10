bd63f9d1c6dde2e3aa3d2d36928bf60342959cc8
alumi <alumi@tigerparty.com>
2015-01-11 00:31:56 +0800

    finish calaulateUpperBound()

---
+++ 01KnapsackProblem/usingFunction.cpp
@@ -0,0 +1,9 @@
+//
+//  usingFunction.cpp
+//  01KnapsackProblem
+//
+//  Created by Lu Kuan-Yin on 1/7/15.
+//  Copyright (c) 2015 NTU. All rights reserved.
+//
+
+#include "usingFunction.h"
---
+++ 01KnapsackProblem/usingFunction.h
@@ -0,0 +1,14 @@
+//
+//  usingFunction.h
+//  01KnapsackProblem
+//
+//  Created by Lu Kuan-Yin on 1/7/15.
+//  Copyright (c) 2015 NTU. All rights reserved.
+//
+
+#ifndef ___1KnapsackProblem__usingFunction__
+#define ___1KnapsackProblem__usingFunction__
+
+#include <stdio.h>
+
+#endif /* defined(___1KnapsackProblem__usingFunction__) */
